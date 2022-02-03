900ec1039c5cd5fed89f0414ea584a3ad81527ac
Max Bergman <maxbergman@outlook.com>
2022-02-03 11:39:55 +0100

    Enhanced comments

--- MagicEnums/Attributes/DisableConcurrencyAttribute.cs
+++ MagicEnums/Attributes/DisableConcurrencyAttribute.cs
@@ -2,7 +2,7 @@
 
 /// <summary>
 /// Add this attribute to the enum to disable concurrency and therefore optimise the memory usage and speed.
-/// Warning! Only use this label when the members are created from a static context.
+/// Warning! Only use this label when you are sure that no race conditions can take place.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
 public sealed class DisableConcurrencyAttribute : Attribute
