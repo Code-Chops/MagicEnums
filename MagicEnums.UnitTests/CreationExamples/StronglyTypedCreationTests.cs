9ff422aee1469d213f6ad2f9a30a55cac0505287
Max Bergman <maxbergman@outlook.com>
2022-09-14 17:37:15 +0200

    2.9.3: Updated package references.

--- MagicEnums.UnitTests/CreationExamples/StronglyTypedCreationTests.cs
+++ MagicEnums.UnitTests/CreationExamples/StronglyTypedCreationTests.cs
@@ -5,27 +5,36 @@ public class StronglyTypedCreationTests
 {
 	public record Vehicle(int WheelCount) : MagicEnum<Vehicle>
 	{
-		public static readonly Vehicle		Default		= CreateMember();
-		public static readonly Type.Bicycle	Bicycle		= CreateMember<Type.Bicycle>();
-		public static readonly Type.Scooter	Scooter		= CreateMember<Type.Scooter>();
-		public static readonly Type.Car		FuelCar		= CreateMember(() => new Type.Car(EmitsCo2: true));
-		public static readonly Type.Car		ElectricCar	= CreateMember(() => new Type.Car(EmitsCo2: false));
-
+		public static readonly Vehicle			Default		= CreateMember();
+		public static readonly Type.Bicycle		Bicycle		= CreateMember<Type.Bicycle>();
+		public static readonly Type.MotorCycle	MotorCycle	= CreateMember<Type.MotorCycle>();
+		public static readonly Type.Car			FuelCar		= CreateMember(() => new Type.Car(EmitsCo2: true));
+		public static readonly Type.Car			ElectricCar	= CreateMember(() => new Type.Car(EmitsCo2: false));
+		
 		public static class Type
 		{
 			public record Bicycle()				: Vehicle(WheelCount: 2);
+			public record MotorCycle()			: Vehicle(WheelCount: 2);
 			public record Car(bool EmitsCo2)	: Vehicle(WheelCount: 4);
-			public record Scooter()				: Vehicle(WheelCount: 2);
-		}
+		} 
 	}
-
+	
 	[Fact]
 	public void Test()
 	{
 		Assert.IsType<Vehicle>(Vehicle.Default);
 		Assert.IsType<Vehicle.Type.Bicycle>(Vehicle.Bicycle);
-		Assert.Equal(2, Vehicle.Scooter.Value);
+		Assert.Equal(2, Vehicle.MotorCycle.Value);
 		Assert.False(Vehicle.ElectricCar.EmitsCo2);
 		Assert.Equal(4, Vehicle.FuelCar.WheelCount);
+
+		var vehicle = (Vehicle)Vehicle.Bicycle;
+
+		var speedingFineInEur = vehicle switch
+		{
+			Vehicle.Type.MotorCycle => 60,
+			Vehicle.Type.Car		=> 100,
+			_						=> 0,
+		};
 	}
 }
