3dffdd28aadc16532e25420c5b92efbceb01d8f0
Max Bergman <maxbergman@outlook.com>
2022-09-16 14:59:19 +0200

    2.9.7: Expanded IMagicEnum core static abstract interface.

--- MagicEnums/Core/IMagicEnumCore.cs
+++ MagicEnums/Core/IMagicEnumCore.cs
@@ -6,6 +6,11 @@ public interface IMagicEnumCore<TSelf, TValue> : IMagicEnum<TValue>, IEnumerable
 	where TSelf : MagicEnumCore<TSelf, TValue>
 	where TValue : IEquatable<TValue>, IComparable<TValue>
 {
+	public static abstract TValue? GetDefaultValue();
+	public static abstract int GetMemberCount();
+	public static abstract int GetUniqueValueCount();
+	public static abstract IEnumerable<TSelf> GetMembers();
+	public static abstract IEnumerable<TValue> GetValues();
 	public static abstract bool TryGetSingleMember(string memberName, [NotNullWhen(true)] out TSelf? member);
 	public static abstract TSelf GetSingleMember(string memberName);
 	public static abstract bool TryGetSingleMember(TValue memberValue, [NotNullWhen(true)] out TSelf? member);
