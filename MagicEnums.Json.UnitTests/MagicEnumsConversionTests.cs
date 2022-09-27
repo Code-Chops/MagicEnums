a7a543f46b8ce4e08100b941bebb83bd199dd1d9
Max Bergman <maxbergman@outlook.com>
2022-09-27 22:55:42 +0200

    2.10.14: Updated packages.

--- MagicEnums.Json.UnitTests/MagicEnumsConversionTests.cs
+++ MagicEnums.Json.UnitTests/MagicEnumsConversionTests.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text.Json;
 using CodeChops.DomainDrivenDesign.DomainModeling.Identities.Serialization.Json;
-using CodeChops.DomainDrivenDesign.DomainModeling.Serialization;
 using CodeChops.Geometry.Space.Directions.Strict;
 using CodeChops.Geometry.Space.Directions.Strict.Definitions;
 using Xunit;
@@ -11,19 +10,24 @@ public class MagicEnumsConversionTests
 {
 	private const string Json = @$"""{nameof(MagicEnumMock1)}.{nameof(MagicEnumMock1.Value2)}""";
 	private const string WrapperJson = @$"{{""{nameof(MagicEnumWrapperContractMock.Enum)}"":""{nameof(MagicEnumMock2)}.{nameof(MagicEnumMock2.Value3)}"",""Direction"":""DiagonalDirection.SouthEast""}}";
-
-	static MagicEnumsConversionTests()
+		
+	private JsonSerializerOptions JsonSerializerOptions { get; }
+	
+	public MagicEnumsConversionTests()
 	{
 		var magicEnums = new IMagicEnum[] { new MagicEnumMock1(), new MagicEnumMock2() }.Concat(StrictDirectionEnum<int>.GetValues().Select(value => value.UninitializedInstance));
-
-		JsonSerialization.DefaultOptions.Converters.Add(new MagicEnumJsonConverterFactory(magicEnums));
-		JsonSerialization.DefaultOptions.Converters.Add(new IdentityJsonConverterFactory());
+		
+		this.JsonSerializerOptions = new()
+		{
+			WriteIndented = false,
+			Converters = { new MagicEnumJsonConverterFactory(magicEnums), new IdentityJsonConverterFactory() }
+		};
 	}
 
 	[Fact]
 	public void Conversion_ToDomainModel_IsCorrect()
 	{
-		var magicEnum = JsonSerializer.Deserialize<MagicEnumMock1>(Json, JsonSerialization.DefaultOptions)!;
+		var magicEnum = JsonSerializer.Deserialize<MagicEnumMock1>(Json, this.JsonSerializerOptions)!;
 		Assert.NotNull(magicEnum);
 		
 		Assert.Equal(typeof(MagicEnumMock1), magicEnum.GetType());
@@ -34,7 +38,7 @@ public class MagicEnumsConversionTests
 	[Fact]
 	public void Deserialization_MagicEnum_IsCorrect()
 	{
-		var contract = JsonSerializer.Deserialize<IMagicEnum>(Json, JsonSerialization.DefaultOptions)!;
+		var contract = JsonSerializer.Deserialize<IMagicEnum>(Json, this.JsonSerializerOptions)!;
 		Assert.NotNull(contract);
 
 		Assert.Equal(typeof(MagicEnumMock1), contract.GetType());
@@ -44,7 +48,7 @@ public class MagicEnumsConversionTests
 	[Fact]
 	public void Serialization_MagicEnum_IsCorrect()
 	{
-		var json = JsonSerializer.Serialize(MagicEnumMock1.Value2, JsonSerialization.DefaultOptions);
+		var json = JsonSerializer.Serialize(MagicEnumMock1.Value2, this.JsonSerializerOptions);
         
 		Assert.Equal(Json, json);
 	}
@@ -53,7 +57,7 @@ public class MagicEnumsConversionTests
 	public void Serialization_MagicEnum_WithWrapper_ShouldWork()
 	{
 		var wrapper = new MagicEnumWrapperContractMock(@enum: MagicEnumMock2.Value3, direction: DiagonalDirection<int>.SouthEast);
-		var json = JsonSerializer.Serialize(wrapper, JsonSerialization.DefaultOptions);
+		var json = JsonSerializer.Serialize(wrapper, this.JsonSerializerOptions);
 		
 		Assert.Equal(WrapperJson, json);
 	}
@@ -61,7 +65,7 @@ public class MagicEnumsConversionTests
 	[Fact]
 	public void Deserialization_MagicEnum_WithWrapper_ShouldWork()
 	{
-		var magicEnum = JsonSerializer.Deserialize<MagicEnumWrapperContractMock>(WrapperJson, JsonSerialization.DefaultOptions)!;
+		var magicEnum = JsonSerializer.Deserialize<MagicEnumWrapperContractMock>(WrapperJson, this.JsonSerializerOptions)!;
 		Assert.NotNull(magicEnum);
 		
 		Assert.Equal(typeof(MagicEnumWrapperContractMock), magicEnum.GetType());
