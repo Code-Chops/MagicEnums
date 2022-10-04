144f609233faf3f60c7367ddb3b77005228fb26f
Max Bergman <maxbergman@outlook.com>
2022-10-04 14:17:46 +0200

    3.0.2: Re-added ForwardInstancesMethodsToStatic.

--- MagicEnums/Core/IMagicEnumCore.cs
+++ MagicEnums/Core/IMagicEnumCore.cs
@@ -1,20 +1,18 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿namespace CodeChops.MagicEnums.Core;
 
-namespace CodeChops.MagicEnums.Core;
-
-public interface IMagicEnumCore<TSelf, TValue> : IEnumerable<TSelf>, IMagicEnum
-	where TSelf : IMagicEnumCore<TSelf, TValue>
+public interface IMagicEnumCore<TSelf, TValue> : IMagicEnum<TValue>, IEnumerable<TSelf>
+	where TSelf : MagicEnumCore<TSelf, TValue>
 	where TValue : IEquatable<TValue>, IComparable<TValue>
 {
-	public static abstract TValue? GetDefaultValue();
-	public static abstract int GetMemberCount();
-	public static abstract int GetUniqueValueCount();
-	public static abstract IEnumerable<TSelf> GetMembers();
-	public static abstract IEnumerable<TValue> GetValues();
+	public new static abstract TValue? GetDefaultValue();
+	public new static abstract int GetMemberCount();
+	public new static abstract int GetUniqueValueCount();
+	public new static abstract IEnumerable<TSelf> GetMembers();
+	public new static abstract IEnumerable<TValue> GetValues();
 	public static abstract bool TryGetSingleMember(string memberName, [NotNullWhen(true)] out TSelf? member);
-	public static abstract TSelf GetSingleMember(string memberName);
+	public new static abstract TSelf GetSingleMember(string memberName);
 	public static abstract bool TryGetSingleMember(TValue memberValue, [NotNullWhen(true)] out TSelf? member);
-	public static abstract TSelf GetSingleMember(TValue memberValue);
+	public new static abstract TSelf GetSingleMember(TValue memberValue);
 	public static abstract bool TryGetMembers(TValue memberValue, [NotNullWhen(true)] out IReadOnlyCollection<TSelf>? members);
-	public static abstract IEnumerable<TSelf> GetMembers(TValue memberValue);
+	public new static abstract IEnumerable<TSelf> GetMembers(TValue memberValue);
 }
--- MagicEnums/Properties/GlobalUsings.cs
+++ MagicEnums/Properties/GlobalUsings.cs
@@ -1,7 +1,7 @@
 ﻿global using System.Runtime.CompilerServices;
 global using System.Runtime.Serialization;
+global using System.Diagnostics.CodeAnalysis;
 
 global using CodeChops.GenericMath;
 global using CodeChops.MagicEnums.Core;
-global using CodeChops.DomainDrivenDesign.DomainModeling;
 global using CodeChops.DomainDrivenDesign.DomainModeling.Identities;
