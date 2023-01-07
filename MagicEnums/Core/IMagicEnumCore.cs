bd1f5beace30297462a152eaf750d1b889858a23
Max Bergman <maxbergman@outlook.com>
2023-01-07 11:34:08 +0100

    Revert "Revert "3.5.0: Removed ForwardInstanceMethodsToStatic as it obsolete now: ImplementationDiscovery generates it.""
    This reverts commit 5d690a60e1bc5da544a68e9472ce0ad68b40790e.

--- MagicEnums/Core/IMagicEnumCore.cs
+++ MagicEnums/Core/IMagicEnumCore.cs
@@ -4,15 +4,15 @@ public interface IMagicEnumCore<TSelf, TValue> : IMagicEnum<TValue>, IId<TSelf, 
 	where TSelf : MagicEnumCore<TSelf, TValue>
 	where TValue : IEquatable<TValue>, IComparable<TValue>
 {
-	public new static abstract TValue? GetDefaultValue();
-	public new static abstract int GetMemberCount();
-	public new static abstract int GetUniqueValueCount();
-	public new static abstract IEnumerable<TSelf> GetMembers();
-	public new static abstract IEnumerable<TValue> GetValues();
+	public static abstract TValue? GetDefaultValue();
+	public static abstract int GetMemberCount();
+	public static abstract int GetUniqueValueCount();
+	public static abstract IEnumerable<TSelf> GetMembers();
+	public static abstract IEnumerable<TValue> GetValues();
 	public static abstract bool TryGetSingleMember(string memberName, [NotNullWhen(true)] out TSelf? member);
-	public new static abstract TSelf GetSingleMember(string memberName);
+	public static abstract TSelf GetSingleMember(string memberName);
 	public static abstract bool TryGetSingleMember(TValue memberValue, [NotNullWhen(true)] out TSelf? member);
-	public new static abstract TSelf GetSingleMember(TValue memberValue);
+	public static abstract TSelf GetSingleMember(TValue memberValue);
 	public static abstract bool TryGetMembers(TValue memberValue, [NotNullWhen(true)] out IReadOnlyCollection<TSelf>? members);
-	public new static abstract IEnumerable<TSelf> GetMembers(TValue memberValue);
+	public static abstract IEnumerable<TSelf> GetMembers(TValue memberValue);
 }
