5d690a60e1bc5da544a68e9472ce0ad68b40790e
Max Bergman <maxbergman@outlook.com>
2023-01-07 11:14:06 +0100

    Revert "3.5.0: Removed ForwardInstanceMethodsToStatic as it obsolete now: ImplementationDiscovery generates it."
    This reverts commit 59691c6a556b2f2e15202e056398998a695fe80e.

--- MagicEnums/IMagicEnum.cs
+++ MagicEnums/IMagicEnum.cs
@@ -1,8 +1,19 @@
 ﻿namespace CodeChops.MagicEnums;
 
-public interface IMagicEnum<out TValue> : IMember<TValue>, IMagicEnum
+public interface IMagicEnum<TValue> : IMember<TValue>, IMagicEnum
 	where TValue : IEquatable<TValue>, IComparable<TValue>
 {
+	TValue? GetDefaultValue();
+	int GetMemberCount();
+	int GetUniqueValueCount();
+	IEnumerable<IMagicEnum<TValue>> GetMembers();
+	IEnumerable<TValue> GetValues();
+	bool TryGetSingleMember(string memberName, [NotNullWhen(true)] out IMagicEnum<TValue>? member);
+	IMagicEnum<TValue> GetSingleMember(string memberName);
+	bool TryGetSingleMember(TValue memberValue, [NotNullWhen(true)] out IMagicEnum<TValue>? member);
+	IMagicEnum<TValue> GetSingleMember(TValue memberValue);
+	bool TryGetMembers(TValue memberValue, [NotNullWhen(true)] out IReadOnlyCollection<IMagicEnum<TValue>>? members);
+	IEnumerable<IMagicEnum<TValue>> GetMembers(TValue memberValue);
 }
 
 public interface IMagicEnum : IMember, IId
